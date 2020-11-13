54c9120293f521b6a99f4ef51c1267c8e099fa0a
Lortseam <lortseam@gmail.com>
2020-11-13 22:14:32 +0100

    Removed some unused imports

--- src/main/java/me/lortseam/completeconfig/ClientConfigManager.java
+++ src/main/java/me/lortseam/completeconfig/ClientConfigManager.java
@@ -2,8 +2,6 @@ package me.lortseam.completeconfig;
 
 import me.lortseam.completeconfig.gui.GuiBuilder;
 import me.lortseam.completeconfig.gui.cloth.ClothGuiBuilder;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.gui.screen.Screen;
 
