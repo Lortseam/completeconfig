315aa6d9355f31eed886543533916a8224aaed77
Lortseam <lortseam@gmail.com>
2020-11-21 19:00:14 +0100

    Fix naming

--- src/main/java/me/lortseam/completeconfig/api/ConfigOwner.java
+++ src/main/java/me/lortseam/completeconfig/api/ConfigOwner.java
@@ -4,14 +4,14 @@ import me.lortseam.completeconfig.ConfigBuilder;
 
 public interface ConfigOwner {
 
-    default void onInitializeConfig(ConfigBuilder creator) {}
+    default void onInitializeConfig(ConfigBuilder builder) {}
 
-    default void onInitializeClientConfig(ConfigBuilder creator) {
-        onInitializeConfig(creator);
+    default void onInitializeClientConfig(ConfigBuilder builder) {
+        onInitializeConfig(builder);
     }
 
-    default void onInitializeServerConfig(ConfigBuilder creator) {
-        onInitializeConfig(creator);
+    default void onInitializeServerConfig(ConfigBuilder builder) {
+        onInitializeConfig(builder);
     }
 
     default String[] getConfigBranch() {
