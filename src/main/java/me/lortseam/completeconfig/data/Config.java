d259eeeb042a9820a156851a94c852891acdadd5
Lortseam <lortseam@gmail.com>
2021-01-11 15:32:30 +0100

    Removed unused field

--- src/main/java/me/lortseam/completeconfig/data/Config.java
+++ src/main/java/me/lortseam/completeconfig/data/Config.java
@@ -2,14 +2,10 @@ package me.lortseam.completeconfig.data;
 
 import me.lortseam.completeconfig.api.ConfigGroup;
 import me.lortseam.completeconfig.data.gui.TranslationIdentifier;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 
 public class Config extends CollectionMap {
-
-    private static final Logger LOGGER = LogManager.getLogger();
 
     public Config(String modID, List<ConfigGroup> topLevelGroups) {
         super(new TranslationIdentifier(modID));
