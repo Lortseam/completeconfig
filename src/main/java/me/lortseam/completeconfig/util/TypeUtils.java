452c0e2fe592c07325e9a482137d57b0ba3da97f
Lortseam <lortseam@gmail.com>
2021-01-23 16:06:15 +0100

    Remove unused logger

--- src/main/java/me/lortseam/completeconfig/util/TypeUtils.java
+++ src/main/java/me/lortseam/completeconfig/util/TypeUtils.java
@@ -2,12 +2,10 @@ package me.lortseam.completeconfig.util;
 
 import com.google.common.reflect.TypeToken;
 import io.leangen.geantyref.GenericTypeReflector;
-import lombok.extern.log4j.Log4j2;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Type;
 
-@Log4j2
 public final class TypeUtils {
 
     public static Type getFieldType(Field field) {
