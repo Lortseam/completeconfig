5032f3c52330cc180a6a2ca90f0cd6a99b3599e9
Lortseam <lortseam@gmail.com>
2021-01-23 16:05:54 +0100

    Remove unused method

--- src/main/java/me/lortseam/completeconfig/util/TypeUtils.java
+++ src/main/java/me/lortseam/completeconfig/util/TypeUtils.java
@@ -3,12 +3,9 @@ package me.lortseam.completeconfig.util;
 import com.google.common.reflect.TypeToken;
 import io.leangen.geantyref.GenericTypeReflector;
 import lombok.extern.log4j.Log4j2;
-import org.spongepowered.configurate.serialize.TypeSerializerCollection;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Type;
-import java.util.Arrays;
-import java.util.Objects;
 
 @Log4j2
 public final class TypeUtils {
@@ -19,25 +16,6 @@ public final class TypeUtils {
 
     public static Class<?> getTypeClass(Type type) {
         return TypeToken.of(type).getRawType();
-    }
-
-    public static TypeSerializerCollection mergeSerializers(TypeSerializerCollection... typeSerializerCollections) {
-        typeSerializerCollections = Arrays.stream(typeSerializerCollections).filter(Objects::nonNull).toArray(TypeSerializerCollection[]::new);
-        switch (typeSerializerCollections.length) {
-            case 0:
-                logger.warn("Tried to merge non-existent type serializer collections!");
-                return null;
-
-            case 1:
-                return typeSerializerCollections[0];
-
-            default:
-                TypeSerializerCollection.Builder builder = TypeSerializerCollection.builder();
-                for (TypeSerializerCollection collection : typeSerializerCollections) {
-                    builder.registerAll(collection);
-                }
-                return builder.build();
-        }
     }
 
 }
