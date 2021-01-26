a707d4918369c86b9fb55bb63bfd53c97226cfc2
Lortseam <lortseam@gmail.com>
2021-01-26 18:11:54 +0100

    Make TypeUtils a UtilityClass

--- src/main/java/me/lortseam/completeconfig/util/TypeUtils.java
+++ src/main/java/me/lortseam/completeconfig/util/TypeUtils.java
@@ -2,10 +2,12 @@ package me.lortseam.completeconfig.util;
 
 import com.google.common.reflect.TypeToken;
 import io.leangen.geantyref.GenericTypeReflector;
+import lombok.experimental.UtilityClass;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Type;
 
+@UtilityClass
 public final class TypeUtils {
 
     public static Type getFieldType(Field field) {
