22766f4f81ce5c5ac89ebeeb62c5c614a22a7122
Lortseam <lortseam@gmail.com>
2021-04-17 13:20:46 +0200

    Remove unused config field from example mod

--- example/src/main/java/me/lortseam/completeconfig/example/ExampleMod.java
+++ example/src/main/java/me/lortseam/completeconfig/example/ExampleMod.java
@@ -1,18 +1,15 @@
 package me.lortseam.completeconfig.example;
 
-import lombok.Getter;
 import me.lortseam.completeconfig.data.Config;
 import net.fabricmc.api.ModInitializer;
 
 public class ExampleMod implements ModInitializer {
 
     public static final String MOD_ID = "example";
-    @Getter
-    private static Config config;
 
     @Override
     public void onInitialize() {
-        config = Config.builder(MOD_ID)
+        Config.builder(MOD_ID)
                 .add(new Settings())
                 .build();
     }
