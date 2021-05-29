4950157be887d495c2e20c5f8d2fb0423416d646
Lortseam <lortseam@gmail.com>
2021-05-29 23:57:54 +0200

    Javadoc

--- lib/src/main/java/me/lortseam/completeconfig/extensions/GuiExtension.java
+++ lib/src/main/java/me/lortseam/completeconfig/extensions/GuiExtension.java
@@ -3,16 +3,16 @@ package me.lortseam.completeconfig.extensions;
 import me.lortseam.completeconfig.gui.cloth.GuiProvider;
 
 /**
- * The main extension type for extending the Cloth Config screen building process.
+ * The main extension type to extend the Cloth Config screen building process.
  *
  * @see me.lortseam.completeconfig.gui.cloth.ClothConfigScreenBuilder
  */
 public interface GuiExtension extends Extension {
 
     /**
-     * Used to register custom {@link GuiProvider}s.
+     * Used to register custom GUI providers.
      *
-     * @return an array of custom {@link GuiProvider}s
+     * @return an array of custom GUI providers
      *
      * @see GuiProvider
      */
