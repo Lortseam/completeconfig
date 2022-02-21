b7c01fc7f02a0915937ce8dfb3452804a82c3248
Lortseam <lortseam@gmail.com>
2022-02-21 21:05:18 +0100

    Update deps

--- lib/src/main/java/me/lortseam/completeconfig/gui/coat/CoatScreenBuilder.java
+++ lib/src/main/java/me/lortseam/completeconfig/gui/coat/CoatScreenBuilder.java
@@ -4,9 +4,9 @@ import com.google.common.collect.Lists;
 import de.siphalor.coat.input.CheckBoxConfigInput;
 import de.siphalor.coat.input.SliderConfigInput;
 import de.siphalor.coat.input.TextConfigInput;
-import de.siphalor.coat.list.ConfigListEntry;
-import de.siphalor.coat.list.ConfigListWidget;
-import de.siphalor.coat.list.entry.ConfigListConfigEntry;
+import de.siphalor.coat.list.complex.ConfigCategoryWidget;
+import de.siphalor.coat.list.entry.ConfigCategoryConfigEntry;
+import de.siphalor.coat.list.entry.ConfigContainerEntry;
 import de.siphalor.coat.screen.ConfigScreen;
 import me.lortseam.completeconfig.data.*;
 import me.lortseam.completeconfig.gui.ConfigScreenBuilder;
@@ -23,47 +23,47 @@ import net.minecraft.text.LiteralText;
 import java.util.ArrayList;
 import java.util.List;
 
-public final class CoatScreenBuilder extends ConfigScreenBuilder<ConfigListConfigEntry<?>> {
+public final class CoatScreenBuilder extends ConfigScreenBuilder<ConfigCategoryConfigEntry<?>> {
 
     // TODO: Add missing providers
-    private static final List<GuiProvider<ConfigListConfigEntry<?>>> globalProviders = Lists.newArrayList(
-            GuiProvider.create(BooleanEntry.class, entry -> new ConfigListConfigEntry<>(
+    private static final List<GuiProvider<ConfigCategoryConfigEntry<?>>> globalProviders = Lists.newArrayList(
+            GuiProvider.create(BooleanEntry.class, entry -> new ConfigCategoryConfigEntry<>(
                     (BaseText) entry.getText(),
                     (BaseText) entry.getDescription().orElse(LiteralText.EMPTY),
                     new BasicEntryHandler<>(entry),
                     new CheckBoxConfigInput(null, entry.getValue(), false)
             ), boolean.class, Boolean.class),
-            GuiProvider.create(SliderEntry.class, (SliderEntry<Integer> entry) -> new ConfigListConfigEntry<>(
+            GuiProvider.create(SliderEntry.class, (SliderEntry<Integer> entry) -> new ConfigCategoryConfigEntry<>(
                     (BaseText) entry.getText(),
                     (BaseText) entry.getDescription().orElse(LiteralText.EMPTY),
                     new BoundedEntryHandler<>(entry),
                     new SliderConfigInput<>(entry.getValue(), entry.getMin(), entry.getMax())
             ), int.class, Integer.class),
-            GuiProvider.create(SliderEntry.class, (SliderEntry<Long> entry) -> new ConfigListConfigEntry<>(
+            GuiProvider.create(SliderEntry.class, (SliderEntry<Long> entry) -> new ConfigCategoryConfigEntry<>(
                     (BaseText) entry.getText(),
                     (BaseText) entry.getDescription().orElse(LiteralText.EMPTY),
                     new BoundedEntryHandler<>(entry),
                     new SliderConfigInput<>(entry.getValue(), entry.getMin(), entry.getMax())
             ), long.class, Long.class),
-            GuiProvider.create(SliderEntry.class, (SliderEntry<Float> entry) -> new ConfigListConfigEntry<>(
+            GuiProvider.create(SliderEntry.class, (SliderEntry<Float> entry) -> new ConfigCategoryConfigEntry<>(
                     (BaseText) entry.getText(),
                     (BaseText) entry.getDescription().orElse(LiteralText.EMPTY),
                     new BoundedEntryHandler<>(entry),
                     new SliderConfigInput<>(entry.getValue(), entry.getMin(), entry.getMax())
             ), float.class, Float.class),
-            GuiProvider.create(SliderEntry.class, (SliderEntry<Double> entry) -> new ConfigListConfigEntry<>(
+            GuiProvider.create(SliderEntry.class, (SliderEntry<Double> entry) -> new ConfigCategoryConfigEntry<>(
                     (BaseText) entry.getText(),
                     (BaseText) entry.getDescription().orElse(LiteralText.EMPTY),
                     new BoundedEntryHandler<>(entry),
                     new SliderConfigInput<>(entry.getValue(), entry.getMin(), entry.getMax())
             ), double.class, Double.class),
-            GuiProvider.create((Entry<String> entry) -> new ConfigListConfigEntry<>(
+            GuiProvider.create((Entry<String> entry) -> new ConfigCategoryConfigEntry<>(
                     (BaseText) entry.getText(),
                     (BaseText) entry.getDescription().orElse(LiteralText.EMPTY),
                     new BasicEntryHandler<>(entry),
                     new TextConfigInput(entry.getValue())
             ), String.class),
-            GuiProvider.create(EnumEntry.class, (EnumEntry<Enum<?>> entry) -> new ConfigListConfigEntry<>(
+            GuiProvider.create(EnumEntry.class, (EnumEntry<Enum<?>> entry) -> new ConfigCategoryConfigEntry<>(
                     (BaseText) entry.getText(),
                     (BaseText) entry.getDescription().orElse(LiteralText.EMPTY),
                     new BasicEntryHandler<>(entry),
@@ -77,13 +77,13 @@ public final class CoatScreenBuilder extends ConfigScreenBuilder<ConfigListConfi
 
     @Override
     public Screen build(Screen parentScreen, Config config) {
-        List<ConfigListWidget> list = new ArrayList<>();
+        List<ConfigCategoryWidget> list = new ArrayList<>();
         if (!config.getEntries().isEmpty()) {
-            List<ConfigListEntry> entries = new ArrayList<>();
+            List<ConfigContainerEntry> entries = new ArrayList<>();
             for (Entry<?> entry : config.getEntries()) {
                 entries.add(buildEntry(entry));
             }
-            list.add(new ConfigListWidget(MinecraftClient.getInstance(), config.getText(), entries, DrawableHelper.OPTIONS_BACKGROUND_TEXTURE));
+            list.add(new ConfigCategoryWidget(MinecraftClient.getInstance(), config.getText(), entries, DrawableHelper.OPTIONS_BACKGROUND_TEXTURE));
         }
         for (Cluster cluster : config.getClusters()) {
             list.add(buildListWidget(cluster));
@@ -91,12 +91,12 @@ public final class CoatScreenBuilder extends ConfigScreenBuilder<ConfigListConfi
         return new ConfigScreen(parentScreen, getTitle(config), list);
     }
 
-    private ConfigListWidget buildListWidget(Cluster cluster) {
-        List<ConfigListEntry> list = new ArrayList<>();
+    private ConfigCategoryWidget buildListWidget(Cluster cluster) {
+        List<ConfigContainerEntry> list = new ArrayList<>();
         for (Entry<?> entry : cluster.getEntries()) {
             list.add(buildEntry(entry));
         }
-        ConfigListWidget widget = new ConfigListWidget(MinecraftClient.getInstance(), cluster.getText(), list, DrawableHelper.OPTIONS_BACKGROUND_TEXTURE);
+        ConfigCategoryWidget widget = new ConfigCategoryWidget(MinecraftClient.getInstance(), cluster.getText(), list, DrawableHelper.OPTIONS_BACKGROUND_TEXTURE);
         for (Cluster subCluster : cluster.getClusters()) {
             widget.addSubTree(buildListWidget(subCluster));
         }
