4a48d159304c0a75a2d9eadd478da1d0bc4679eb
Lortseam <lortseam@gmail.com>
2022-06-01 18:51:30 +0200

    Javadoc

--- gui-coat/src/main/java/me/lortseam/completeconfig/gui/coat/CoatGuiExtension.java
+++ gui-coat/src/main/java/me/lortseam/completeconfig/gui/coat/CoatGuiExtension.java
@@ -8,6 +8,11 @@ import java.util.Collection;
 
 public interface CoatGuiExtension extends Extension {
 
+    /**
+     * Used to register global GUI providers.
+     *
+     * @return a collection of GUI providers
+     */
     default Collection<GuiProvider<ConfigCategoryConfigEntry<?>>> getProviders() {
         return null;
     }
