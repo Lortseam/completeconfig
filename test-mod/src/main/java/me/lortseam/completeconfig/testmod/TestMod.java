5a9f8bfa6017beb10601995408223c4269a8ac46
Lortseam <lortseam@gmail.com>
2022-06-06 20:01:04 +0200

    Remove unused class in test mod

--- test-mod/src/main/java/me/lortseam/completeconfig/testmod/TestMod.java
+++ test-mod/src/main/java/me/lortseam/completeconfig/testmod/TestMod.java
@@ -1,7 +1,16 @@
 package me.lortseam.completeconfig.testmod;
 
-public class TestMod {
+import me.lortseam.completeconfig.testmod.config.ModConfig;
+import net.fabricmc.api.DedicatedServerModInitializer;
+
+public class TestMod implements DedicatedServerModInitializer {
 
     public static final String MOD_ID = "completeconfig-test-mod";
 
+    @Override
+    public void onInitializeServer() {
+        ModConfig config = new ModConfig();
+        config.load();
+    }
+    
 }
