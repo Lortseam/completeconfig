7eacfa8aac8340c13a0b92f74ce405bcc1e753bc
Lortseam <lortseam@gmail.com>
2022-09-24 18:02:02 +0200

    Revert "[WIP] YACL screen builder"
    This reverts commit b22e3d6178da91a5d11e32fd86002b0d4cf66ffb.

--- gui-yacl/src/main/java/me/lortseam/completeconfig/gui/yacl/ControllerFunction.java
+++
@@ -1,9 +0,0 @@
-package me.lortseam.completeconfig.gui.yacl;
-
-import dev.isxander.yacl.api.Controller;
-import dev.isxander.yacl.api.Option;
-
-import java.util.function.Function;
-
-@FunctionalInterface
-public interface ControllerFunction<T> extends Function<Option<T>, Controller<T>> {}
