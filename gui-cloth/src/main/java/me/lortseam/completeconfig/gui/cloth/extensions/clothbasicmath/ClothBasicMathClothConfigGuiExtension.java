2de5800bed3ac5207c0411799d28bbcb14582b5c
Lortseam <lortseam@gmail.com>
2023-02-11 14:48:58 +0100

    Use AbstractConfigListEntry for ClothConfig entries

--- gui-cloth/src/main/java/me/lortseam/completeconfig/gui/cloth/extensions/clothbasicmath/ClothBasicMathClothConfigGuiExtension.java
+++ gui-cloth/src/main/java/me/lortseam/completeconfig/gui/cloth/extensions/clothbasicmath/ClothBasicMathClothConfigGuiExtension.java
@@ -3,8 +3,8 @@ package me.lortseam.completeconfig.gui.cloth.extensions.clothbasicmath;
 import me.lortseam.completeconfig.data.ColorEntry;
 import me.lortseam.completeconfig.gui.GuiProvider;
 import me.lortseam.completeconfig.gui.cloth.ClothConfigGuiExtension;
+import me.shedaniel.clothconfig2.api.AbstractConfigListEntry;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
-import me.shedaniel.clothconfig2.impl.builders.FieldBuilder;
 import me.shedaniel.math.Color;
 import net.minecraft.text.Text;
 
@@ -13,13 +13,14 @@ import java.util.List;
 public final class ClothBasicMathClothConfigGuiExtension implements ClothConfigGuiExtension {
 
     @Override
-    public List<GuiProvider<FieldBuilder<?, ?, ?>>> getProviders() {
+    public List<GuiProvider<AbstractConfigListEntry<?>>> getProviders() {
         return List.of(GuiProvider.create(ColorEntry.class, (ColorEntry<Color> entry) -> ConfigEntryBuilder.create()
                         .startColorField(entry.getName(), entry.getValue())
                         .setAlphaMode(entry.isAlphaMode())
                         .setDefaultValue(entry.getDefaultValue().getColor())
                         .setTooltip(entry.getDescription().map(description -> new Text[]{description}))
-                        .setSaveConsumer2(entry::setValue),
+                        .setSaveConsumer2(entry::setValue)
+                        .build(),
                 Color.class));
     }
 
