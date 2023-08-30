d2dddf992852f81d89907bf001a2e7efdf0bb21a
Lortseam <lortseam@gmail.com>
2023-08-30 20:28:36 +0200

    Code improvements

--- gui-yacl/src/main/java/me/lortseam/completeconfig/gui/yacl/controller/ListController.java
+++ gui-yacl/src/main/java/me/lortseam/completeconfig/gui/yacl/controller/ListController.java
@@ -6,7 +6,6 @@ import dev.isxander.yacl3.api.controller.ControllerBuilder;
 import dev.isxander.yacl3.api.utils.Dimension;
 import dev.isxander.yacl3.gui.AbstractWidget;
 import dev.isxander.yacl3.gui.YACLScreen;
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import me.lortseam.completeconfig.gui.yacl.ControllerFunction;
@@ -14,7 +13,7 @@ import net.minecraft.text.Text;
 
 import java.util.List;
 
-@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@RequiredArgsConstructor
 public class ListController<T> implements Controller<List<T>> {
 
     public static <T> ControllerBuilder<List<T>> createBuilder(ControllerFunction<T> elementControllerBuilder, T initialValue) {
